2f87a1ac49e2107bcac323a107e1fe7338fd92ef
Si Xu <u6714758@anu.edu.au>
2019-10-12 16:27:13 +1100

    finish the function of add item

--- ListCrown/app/src/main/java/au/anu/u6807681/listcrown/Item.java
+++ ListCrown/app/src/main/java/au/anu/u6807681/listcrown/Item.java
@@ -1,12 +1,21 @@
 package au.anu.u6807681.listcrown;
 
+import java.util.Date;
+
 public class Item {
     public String keyword;
     public String description;
-    public Date startDate;
+    public Date startDate = new Date();
     public Date endDate;
     public String reminderTime;
     public ImportantColor importantLevel;
     public String state;
     public String location;
+    boolean finished = false;
+
+    // unfinished
+    public Item(String keyword, String description) {
+        this.keyword = keyword;
+        this.description = description;
+    }
 }
--- ListCrown/app/src/main/java/au/anu/u6807681/listcrown/MainActivity.java
+++ ListCrown/app/src/main/java/au/anu/u6807681/listcrown/MainActivity.java
@@ -2,12 +2,38 @@ package au.anu.u6807681.listcrown;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     *   include all items
+     */
+    private ArrayList<Item> itemList = new ArrayList<>();
+    private ItemAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //initListItems();
+    }
+
+    public void initListItems() {
+        for (int i = 0; i < 2; i++) {
+            Item addItem = new Item("test "+itemList.size(), "this is test "+itemList.size());
+            itemList.add(addItem);
+        }
+    }
+
+    public void clickAdd(View V) {
+        Item addItem = new Item("test "+itemList.size(), "this is test "+itemList.size());
+        itemList.add(addItem);
+        adapter = new ItemAdapter(itemList, MainActivity.this);
+        ListView listView = (ListView) findViewById(R.id.itemList);
+        listView.setAdapter(adapter);
     }
 }
