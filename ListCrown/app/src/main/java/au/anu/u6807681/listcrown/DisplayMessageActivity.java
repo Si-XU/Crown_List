dba095e819fe74ab6a38eaf845c7edbf3967bb59
Si Xu <u6714758@anu.edu.au>
2019-10-14 13:04:05 +1100

    just commit

---
+++ ListCrown/app/src/main/java/au/anu/u6807681/listcrown/DisplayMessageActivity.java
@@ -0,0 +1,57 @@
+package au.anu.u6807681.listcrown;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+
+    @Override
+    protected void initData() {
+
+        int year = mCalendarView.getCurYear();
+        int month = mCalendarView.getCurMonth();
+
+        Map<String, Calendar> map = new HashMap<>();
+        map.put(getSchemeCalendar(year, month, 3, 0xFF40db25, "假").toString(),
+                getSchemeCalendar(year, month, 3, 0xFF40db25, "假"));
+//        map.put(getSchemeCalendar(year, month, 6, 0xFFe69138, "事").toString(),
+//                getSchemeCalendar(year, month, 6, 0xFFe69138, "事"));
+//        map.put(getSchemeCalendar(year, month, 9, 0xFFdf1356, "议").toString(),
+//                getSchemeCalendar(year, month, 9, 0xFFdf1356, "议"));
+//        map.put(getSchemeCalendar(year, month, 13, 0xFFedc56d, "记").toString(),
+//                getSchemeCalendar(year, month, 13, 0xFFedc56d, "记"));
+//        map.put(getSchemeCalendar(year, month, 14, 0xFFedc56d, "记").toString(),
+//                getSchemeCalendar(year, month, 14, 0xFFedc56d, "记"));
+//        map.put(getSchemeCalendar(year, month, 15, 0xFFaacc44, "假").toString(),
+//                getSchemeCalendar(year, month, 15, 0xFFaacc44, "假"));
+//        map.put(getSchemeCalendar(year, month, 18, 0xFFbc13f0, "记").toString(),
+//                getSchemeCalendar(year, month, 18, 0xFFbc13f0, "记"));
+//        map.put(getSchemeCalendar(year, month, 25, 0xFF13acf0, "假").toString(),
+//                getSchemeCalendar(year, month, 25, 0xFF13acf0, "假"));
+//        map.put(getSchemeCalendar(year, month, 27, 0xFF13acf0, "多").toString(),
+//                getSchemeCalendar(year, month, 27, 0xFF13acf0, "多"));
+        //此方法在巨大的数据量上不影响遍历性能，推荐使用
+        mCalendarView.setSchemeDate(map);
+
+    }
+
+    private Calendar getSchemeCalendar(int year, int month, int day, int color, String text) {
+        Calendar calendar = new Calendar();
+        calendar.setYear(year);
+        calendar.setMonth(month);
+        calendar.setDay(day);
+        calendar.setSchemeColor(color);//如果单独标记颜色、则会使用这个颜色
+        calendar.setScheme(text);
+        return calendar;
+    }
+}
--- ListCrown/app/src/main/java/au/anu/u6807681/listcrown/MainActivity.java
+++ ListCrown/app/src/main/java/au/anu/u6807681/listcrown/MainActivity.java
@@ -1,16 +1,32 @@
 package au.anu.u6807681.listcrown;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.CalendarContract;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
     /**
-     *   include all items
+     * include all items
      */
     private ArrayList<Item> itemList = new ArrayList<>();
     private ItemAdapter adapter;
@@ -19,21 +35,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //initListItems();
     }
 
-    public void initListItems() {
+    public void initListItems() throws ParseException {
+        String[] dateString = {"2019-10-02","2018-10-23","2018-10-15"};
+
         for (int i = 0; i < 2; i++) {
-            Item addItem = new Item("test "+itemList.size(), "this is test "+itemList.size());
+            Item addItem = new Item("test " + itemList.size(), "this is test " + itemList.size());
+            addItem.endDate = new SimpleDateFormat("yyyy-MM-dd").parse(dateString[i]);
             itemList.add(addItem);
         }
     }
 
-    public void clickAdd(View V) {
-        Item addItem = new Item("test "+itemList.size(), "this is test "+itemList.size());
+    public void clickAdd(View V) throws ParseException {
+        Item addItem = new Item("test " + itemList.size(), "this is test " + itemList.size());
+        addItem.endDate = new SimpleDateFormat("yyyy-MM-dd").parse("2018-10-15");
         itemList.add(addItem);
         adapter = new ItemAdapter(itemList, MainActivity.this);
         ListView listView = (ListView) findViewById(R.id.itemList);
         listView.setAdapter(adapter);
     }
+
+    public void clickCalendar(View V) {
+        CalendarView calendar = findViewById(R.id.calendar);
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        startActivity(intent);
+    }
+
 }
