238e6d575a89ec25228baeddd20fee4f87ee58a9
Yun Huang <u6766683@anu.edu.au>
2019-10-17 16:02:23 +1100

    Reminder version2(debug only can set one reminder a time)

---
+++ ListCrown/app/src/main/java/au/anu/u6807681/listcrown/Notification.java
@@ -0,0 +1,75 @@
+package au.anu.u6807681.listcrown;
+
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
+
+//@author Yun Huang
+//class that receive broadcast and send notification
+public class Notification extends BroadcastReceiver  {
+    NotificationManager notificationManager;
+    DatabaseManager databaseManager;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //unique ID for every item
+        long id = 0;
+        id = intent.getLongExtra("id",id);
+        databaseManager = new DatabaseManager(context);
+        databaseManager.open();
+        Cursor cursor = databaseManager.selectToModify(id);
+        String keyword = cursor.getString(cursor.getColumnIndex("keyword"));
+        String importance = cursor.getString(cursor.getColumnIndex("importance"));
+        String state = cursor.getString(cursor.getColumnIndex("state"));
+        //initialise a notification
+        NotificationCompat.Builder notification;
+        notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        //choose a proper channel to send a notification according to importance level of the item
+        //customize characteristic for every channel
+        if (state.equals("undone")){
+        switch (""+importance){
+            case "high" :
+                notification = new NotificationCompat.Builder(context, "H");
+                notification.setContentTitle("Important!!!");
+                notification.setContentText("" +keyword  +" is due");
+                notification.setWhen(System.currentTimeMillis());
+                notification.setSmallIcon(R.drawable.ic_launcher_background);
+                notification.setAutoCancel(true);
+                //notification.setColor(Color.RED);
+                //notification.setLargeIcon();
+                notificationManager.notify((int) id, notification.build());
+                break;
+            case "medium" :
+                notification = new NotificationCompat.Builder(context, "M");
+                notification.setContentTitle("Reminder!");
+                notification.setContentText("" +keyword  +" is due");
+                notification.setWhen(System.currentTimeMillis());
+                notification.setSmallIcon(R.drawable.ic_launcher_background);
+                notification.setAutoCancel(true);
+                //notification.setColor(Color.BLUE);
+                //notification.setLargeIcon();
+                notificationManager.notify((int) id, notification.build());
+                break;
+            case "low" :
+                notification = new NotificationCompat.Builder(context, "L");
+                notification.setContentTitle("Reminder");
+                notification.setContentText("" +keyword  +" is due");
+                notification.setWhen(System.currentTimeMillis());
+                notification.setSmallIcon(R.drawable.ic_launcher_background);
+                notification.setAutoCancel(true);
+                //notification.setColor(Color.BLACK);
+                //notification.setLargeIcon();
+                notificationManager.notify((int) id, notification.build());
+                break;
+        }
+        }
+    }
+}
